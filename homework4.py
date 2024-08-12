bd4850d932c56aeb59744ba723cdb91601ec16b9
Veronika Darst <concitta@yandex.ru>
2024-08-12 14:22:05 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,5 @@
+homeworks = 12
+time = 1.5
+name = 'Python'
+homework = [time/homeworks]
+print('Курс - ',name,', ', 'всего задач: ',homeworks,', ','затрачено часов: ',time,', среднее время выполнения -',homework,'.')
